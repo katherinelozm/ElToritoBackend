04081e4f411edb3042358b7ebab193945b8a9ba2
Katherine Lozano <katherinelozm@gmail.com>
2015-12-08 08:53:09 -0600

    Backend only

---
+++ routes.js
@@ -0,0 +1,10 @@
+var userController = require('./controllers/userController');
+var authController = require('./controllers/authController');
+
+exports.endpoints = [
+	{method: 'GET', path: '/', config: {handler: function(request, reply){reply('API v1, Users')}}},
+	{method: 'POST', path: '/v1/register', config: userController.createUser},
+	{method: 'GET', path: '/v1/users', config: userController.getUsers},
+	{method: 'POST', path: '/v1/login', config: authController.login},
+	{method: 'GET', path: '/v1/logout', config: authController.logout}
+];
